8108b2402d1d7f9a7a06ba91f4b4d67d5546deb3
Maria Eugenia <mariaeugenia.98@hotmail.com>
2024-05-28 11:21:41 +0800

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+
+export * from './ui/top-menu/TopMenu';
+export * from './ui/not-found/PageNotFound';
+export * from './ui/title/Title';
+export * from './ui/sidebar/Sidebar';
+
+export * from './product/size-selector/SizeSelector';
+export * from './product/quantity-selector/QuantitySelector';
+
+export * from './products/product-grid/ProductGrid';
