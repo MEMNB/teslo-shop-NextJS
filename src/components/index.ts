2f9b6ba6c7ef8262ccf0ba5bc6975ae7c48b85d3
Maria Eugenia <mariaeugenia.98@hotmail.com>
2024-05-29 11:25:34 +0800

    Pantalla pedidos

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,5 +6,7 @@ export * from './ui/sidebar/Sidebar';
 
 export * from './product/size-selector/SizeSelector';
 export * from './product/quantity-selector/QuantitySelector';
+export * from './product/slideshow/ProductSlideshow';
+export * from './product/slideshow/ProductMobileSlideshow';
 
 export * from './products/product-grid/ProductGrid';
